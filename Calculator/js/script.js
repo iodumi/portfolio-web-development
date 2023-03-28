1b2d731d16c5b2005591a230653e8a7d1e065943
iodumi <128703908+iodumi@users.noreply.github.com>
2023-03-28 21:25:37 +0300

    Add files via upload

--- Calculator/js/script.js
+++ Calculator/js/script.js
@@ -1,5 +1,5 @@
 function on_load_func() {
-	var flag_operator;	//0- +; 1 - -; 2 - *; 3 - /
+	flag_operator=0;	//0- +; 1 - -; 2 - *; 3 - /
 	flag=0;	//0 - if no number in construction ; 1 - if number in construction
 	flag_xy=1;	//1 - if first number in construction ; 2 - if second number in construction
 	flag_dot1=0;	//0 - if no dot is in number; 1 - if dot in number
@@ -9,12 +9,41 @@ function on_load_func() {
 	count_after_dot2=10;
 	y=0;
 	z=0;
+		
+}
+
+function check_textarea(){
+	var a;
+	a=document.getElementById("myTextarea").value;	//or a=event.keyCode
+//	document.addEventListener("keyup", function(event)
+//	{if (event.keyCode===13) {
+//		console.log("=");
+//		receive_operator_equal();}
+//	}
+//	})
+	if (event.keyCode===13) {
+	receive_operator_equal();
+	}
+	else if (a>=0 && a<=9) {
+		console.log("a09", a);
+	receive_and_display(a);
+	}
+	else if (a=="+") {receive_operator_plus();}
+	else if (a=="-") {receive_operator_minus();}
+	else if (a=="*") {receive_operator_multiply();}
+	else if (a=="/") {receive_operator_divide();}
+	else if (a=="=") {receive_operator_equal();}
+	else if (a=="." || a==",") {receive_dot();}
+	document.getElementById("myTextarea").value='';
 }
 
 function receive_and_display(val) {
 	var x;
 	
 	x=val;
+	
+	console.log("func rec_and_disp x1", x);
+	
 	if (flag_xy==1) {
 	if (!isNaN(x))	//if x is a number
 	{
@@ -23,16 +52,16 @@ function receive_and_display(val) {
 			flag=1;
 		}
 		else if (flag_dot1==0){	
-			y=flag_sign*(10*Math.abs(y)+x);
+			y=flag_sign*(10*Math.abs(y)+(+x));
 			}
 			else{
-				y=flag_sign*(count_after_dot1*(Math.abs(y))+x)/count_after_dot1;
+				y=flag_sign*(count_after_dot1*(Math.abs(y))+(+x))/count_after_dot1;
 				count_after_dot1=count_after_dot1*10;
 			}
 	}
 	else {
 		flag=0;
-		sig=x;
+//		sig=x;
 		flag_xy=2;
 	}
 	document.getElementById("display").innerHTML=y;
@@ -46,10 +75,10 @@ function receive_and_display(val) {
 			flag=1;
 		}
 		else if (flag_dot2==0){	
-			z=flag_sign*(10*Math.abs(z)+x);
+			z=flag_sign*(10*Math.abs(z)+(+x));
 			}
 			else{
-				z=flag_sign*(count_after_dot2*(Math.abs(z))+x)/count_after_dot2;
+				z=flag_sign*(count_after_dot2*(Math.abs(z))+(+x))/count_after_dot2;
 				count_after_dot2=count_after_dot2*10;
 			}
 	}
@@ -75,6 +104,17 @@ function receive_dot() {
 
 function change_sign() {
 	flag_sign=flag_sign*(-1);
+	if (flag_xy==1)
+	{
+		y=flag_sign*y;
+		document.getElementById("display").innerHTML=y;
+	}
+	else if (flag_xy==2)
+	{
+		z=flag_sign*z;
+		document.getElementById("display").innerHTML=z;
+	}
+	else {alert("error change_sign function");}
 }
 
 function receive_operator_plus() {
@@ -102,6 +142,7 @@ function receive_operator_divide() {
 }
 
 function receive_operator_equal() {
+	console.log("=func y", y, "z", z, "flag_operator", flag_operator);
 	var mult=1;
 	if (flag_dot1==1 || flag_dot2==1)
 	{
@@ -115,16 +156,17 @@ function receive_operator_equal() {
 		}
 	}
 	if (flag_operator==0) {
-		document.getElementById("display").innerHTML=(mult*y+mult*z)/mult;
+		console.log("here");
+		document.getElementById("display").innerHTML=(mult*(+y)+mult*(+z))/mult;
 	}
 	else if (flag_operator==1) {
-		document.getElementById("display").innerHTML=(mult*y-mult*z)/mult;
+		document.getElementById("display").innerHTML=(mult*(+y)-mult*(+z))/mult;
 	}
 	else if (flag_operator==2) {
-		document.getElementById("display").innerHTML=(mult*y*z)/mult;
+		document.getElementById("display").innerHTML=(mult*(+y)*(+z))/mult;
 	}
 	else if (flag_operator==3) {
-		document.getElementById("display").innerHTML=(mult*y)/(mult*z);
+		document.getElementById("display").innerHTML=(mult*(+y))/(mult*(+z));
 	}
 	else {alert("error2");
 	}
